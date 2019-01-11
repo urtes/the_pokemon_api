b0a5bb0d45a110160f9264fc64271e10ce75a427
Urte <urte.rimsaite@gmail.com>
2019-01-11 13:38:42 +0200

    remove build.gradle .gitignore

--- src/main/java/wgt/thepokemonapi/CsvLoader.java
+++ src/main/java/wgt/thepokemonapi/CsvLoader.java
@@ -1,4 +1,13 @@
 package wgt.thepokemonapi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CsvLoader {
+
+    public Map<String, ThePokemon> uploadCvs() {
+        Map<String, ThePokemon> pokemonMap = new HashMap<>();
+
+        return pokemonMap;
+    }
 }
