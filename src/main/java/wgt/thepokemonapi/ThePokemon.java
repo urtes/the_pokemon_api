e6dfaf8d5fbf1d2c7e7a670ece78016ff2aba830
Urte <urte.rimsaite@gmail.com>
2019-01-11 16:33:32 +0200

    Not able to set values for thePokemon in CvsReader

--- src/main/java/wgt/thepokemonapi/ThePokemon.java
+++ src/main/java/wgt/thepokemonapi/ThePokemon.java
@@ -2,19 +2,21 @@ package wgt.thepokemonapi;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+
 @Data
 public class ThePokemon {
     private Integer id;
     private String name;
     private String type1;
-    private String type2;
-    private Integer total;
-    private Integer hp;
-    private Integer attack;
-    private Integer defense;
-    private Integer spAtk;
-    private Integer spDef;
-    private Integer speed;
-    private Integer generation;
-    private Boolean legendary;
+//    private String type2;
+//    private Integer total;
+//    private Integer hp;
+//    private Integer attack;
+//    private Integer defense;
+//    private Integer spAtk;
+//    private Integer spDef;
+//    private Integer speed;
+//    private Integer generation;
+//    private Boolean legendary;
 }
--- src/main/java/wgt/thepokemonapi/ThePokemonApiApplication.java
+++ src/main/java/wgt/thepokemonapi/ThePokemonApiApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThePokemonApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ThePokemonApiApplication.class, args);
+
+        CsvReader cvsReader = new CsvReader();
+        cvsReader.readFromCvs();
     }
 
 }
