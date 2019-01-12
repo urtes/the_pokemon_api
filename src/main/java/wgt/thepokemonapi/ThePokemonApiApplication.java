f9da745dffd7f077814a8e7ff02a4b85cc69392f
Urte <urte.rimsaite@gmail.com>
2019-01-12 17:25:09 +0200

    use jackson-dataformat-csv for csv reading

--- src/main/java/wgt/thepokemonapi/ThePokemonApiApplication.java
+++ src/main/java/wgt/thepokemonapi/ThePokemonApiApplication.java
@@ -1,7 +1,19 @@
 package wgt.thepokemonapi;
 
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class ThePokemonApiApplication {
@@ -9,10 +21,31 @@ public class ThePokemonApiApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ThePokemonApiApplication.class, args);
-
-        CsvReader cvsReader = new CsvReader();
-        cvsReader.readFromCvs();
     }
 
+    @Bean
+    public InputStream pokemonStream() throws IOException {
+
+        File initialFile = new File("src/main/resources/static/pokemons.csv");
+        InputStream stream = new FileInputStream(initialFile);
+
+        return stream;
+    }
+
+    @Bean
+    public Map<String, Pokemon> pokemons(InputStream stream) throws IOException {
+
+        Map<String, Pokemon> pokemonMap = new HashMap<>();
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = mapper.schemaFor(Pokemon.class).withHeader().withColumnReordering(true);
+        ObjectReader reader = mapper.readerFor(Pokemon.class).with(schema);
+        List<Pokemon> pokemonList = reader.<Pokemon>readValues(stream).readAll();
+
+        for (Pokemon pokemon : pokemonList) {
+            pokemonMap.put(pokemon.getName(), pokemon);
+        }
+
+        return pokemonMap;
+    }
 }
 
--- src/main/java/wgt/thepokemonapi/ThePokemonController.java
+++ src/main/java/wgt/thepokemonapi/ThePokemonController.java
@@ -1,10 +1,16 @@
 package wgt.thepokemonapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+
 @Controller
 public class ThePokemonController {
+
+    @Autowired
+    private Map<String, Pokemon> pokemonMap;
 
     @GetMapping("/choose-pokemons")
     public String displayPokemonForm() {
