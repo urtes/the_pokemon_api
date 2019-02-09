1a9bb0de6d95c0f6f7c9d022e8f7c9213622658f
Urte <urte.rimsaite@gmail.com>
2019-02-09 16:25:32 +0200

    fixed BattleSystemTest

--- engine/src/test/java/wgt/pokemonapi/BattleSystemTests.java
+++ engine/src/test/java/wgt/pokemonapi/BattleSystemTests.java
@@ -7,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import wgt.pokemonapi.requests.BattleRequest;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
@@ -16,11 +15,11 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class BattleSystemTests {
 
-//    @Mock
-    Map<String, Pokemon> pokemonMap = new HashMap<>();
+    @Mock
+    Map<String, Pokemon> pokemonMap;
 
-//    @InjectMocks
-    BattleSystem battleSystem = new BattleSystem();
+    @InjectMocks
+    BattleSystem battleSystem;
 
     BattleRequest battleRequest = new BattleRequest();
     Pokemon firstToAttack = new Pokemon();
@@ -36,17 +35,16 @@ public class BattleSystemTests {
         firstToAttack.setHealth(100);
         firstToAttack.setAttack(100);
         firstToAttack.setDefense(100);
+        firstToAttack.setAttackSpeed(100);
 
         secondToAttack.setName("secondToAttack");
         secondToAttack.setHealth(20);
         secondToAttack.setAttack(20);
         secondToAttack.setDefense(20);
+        secondToAttack.setAttackSpeed(20);
 
-//        when(pokemonMap.get(battleRequest.getNameOfPokemonA())).thenReturn(firstToAttack);
-//        when(pokemonMap.get(battleRequest.getNameOfPokemonB())).thenReturn(secondToAttack);
-
-        pokemonMap.put(firstToAttack.getName(), firstToAttack);
-        pokemonMap.put(secondToAttack.getName(), secondToAttack);
+        when(pokemonMap.get(battleRequest.getNameOfPokemonA())).thenReturn(firstToAttack);
+        when(pokemonMap.get(battleRequest.getNameOfPokemonB())).thenReturn(secondToAttack);
 
         assertTrue("firstToAttack".equals(battleSystem.fight(battleRequest).getName()));
     }
@@ -61,11 +59,13 @@ public class BattleSystemTests {
         firstToAttack.setHealth(100);
         firstToAttack.setAttack(100);
         firstToAttack.setDefense(100);
+        firstToAttack.setAttackSpeed(100);
 
         secondToAttack.setName("secondToAttack");
         secondToAttack.setHealth(100);
         secondToAttack.setAttack(100);
         secondToAttack.setDefense(100);
+        secondToAttack.setAttackSpeed(100);
 
         when(pokemonMap.get(battleRequest.getNameOfPokemonA())).thenReturn(firstToAttack);
         when(pokemonMap.get(battleRequest.getNameOfPokemonB())).thenReturn(secondToAttack);
